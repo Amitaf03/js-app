4e8d9471a13c4a1296a93b0a3532d64a69cc9800
unknown <fatimaezahraouahmid@gmail.com>
2024-06-18 18:45:11 +0100

    connect API

--- client/src/scenes/dashboard/Row1.tsx
+++ client/src/scenes/dashboard/Row1.tsx
@@ -2,11 +2,11 @@
 import DashboardBox from "../../components/DashboardBox";
 import { useGetKpisQuery } from "@/state/api";
 type Props = {}
-console.log("hello")
+
 const Row1 = () => {
   
   const {data}= useGetKpisQuery();
-  console.log("VITE_BASE_URL:", import.meta.env.VITE_BASE_URL);
+  
   console.log("data:", data) ;
 
   
